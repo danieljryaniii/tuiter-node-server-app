a84bdb52dd1d8cb61166200300d4a2878c55eb89
danieljryaniii <danieljryaniii@gmail.com>
2023-04-02 14:59:35 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import HelloController from "./controllers/hello-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+console.log("Server Started.............");
+console.log("Listening on Port 4000.....");
+
+TuitsController(app);
+HelloController(app);
+UsersController(app);
+app.listen(process.env.PORT || 4000);
+
